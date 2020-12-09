c42924b7b2c9ac374ddfa63d2e2add2df85d7de4
GPW <GPW@BST-R11>
2020-12-09 22:41:14 +0530

    add Date Picker Renderer for iOS

--- Qloudid.iOS/Renderers/CustomDatePickerRenderer.cs
+++ Qloudid.iOS/Renderers/CustomDatePickerRenderer.cs
@@ -1,13 +1,19 @@
-﻿using Foundation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UIKit;
+﻿using UIKit;
+using Xamarin.Forms;
+using Qloudid.Controls;
+using Qloudid.iOS.Renderers;
+using Xamarin.Forms.Platform.iOS;
 
+[assembly: ExportRenderer(typeof(CustomDatePicker), typeof(CustomDatePickerRenderer))]
 namespace Qloudid.iOS.Renderers
 {
-	class CustomDatePickerRenderer
+	public class CustomDatePickerRenderer : DatePickerRenderer
 	{
+		protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+		{
+			base.OnElementChanged(e);
+			Control.Layer.BorderWidth = 0;
+			Control.BorderStyle = UITextBorderStyle.None;
+		}
 	}
 }
