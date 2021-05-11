855efe703d083703456769877d1e52a86ece81a4
GPW <GPW@BST-R11>
2021-05-11 16:52:30 +0530

    Change design Varifie password & Sign In from web page and implement poppins font family

--- Qloudid/Helper/QloudidAppFlatIcons.cs
+++ Qloudid/Helper/QloudidAppFlatIcons.cs
@@ -31,5 +31,6 @@
 		public const string ChevronDown = "\U000f0140";
 		public const string ChevronUp = "\U000f0143";
 		public const string Help = "\U000f02d6";
+		public const string BackspaceOutline = "\U000f0b5c";
 	}
 }
