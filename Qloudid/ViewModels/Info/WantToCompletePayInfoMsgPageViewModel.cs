53d32b5858d14b04d0ac371cc31feb8871f0cd2d
GPW <GPW@BST-R11>
2021-05-15 12:43:31 +0530

    Design new page "WantToCompletePayInfo" & "WantToCompleteCheckInInfo"

---
+++ Qloudid/ViewModels/Info/WantToCompletePayInfoMsgPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+using System.Windows.Input;
+using System.Threading.Tasks;
+
+namespace Qloudid.ViewModels
+{
+	public class WantToCompletePayInfoMsgPageViewModel : BaseViewModel
+	{
+		#region Constructor.
+		public WantToCompletePayInfoMsgPageViewModel(INavigation navigation)
+		{
+			Navigation = navigation;
+		}
+		#endregion
+
+		#region Continue Command.
+		private ICommand continueCommand;
+		public ICommand ContinueCommand
+		{
+			get => continueCommand ?? (continueCommand = new Command(async () => await ExecuteContinueCommand()));
+		}
+		private async Task ExecuteContinueCommand()
+		{
+			await Task.CompletedTask;
+		}
+		#endregion
+
+		#region Cancel Command.
+		private ICommand cancelCommand;
+		public ICommand CancelCommand
+		{
+			get => cancelCommand ?? (cancelCommand = new Command(async () => await ExecuteCancelCommand()));
+		}
+		private async Task ExecuteCancelCommand()
+		{
+			await Task.CompletedTask;
+		}
+		#endregion
+	}
+}
---
+++ Qloudid/Views/Info/WantToCompletePayInfoMsgPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Qloudid.ViewModels;
+
+namespace Qloudid.Views.Info
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WantToCompletePayInfoMsgPage : ContentPage
+	{
+		WantToCompletePayInfoMsgPageViewModel viewModel;
+		public WantToCompletePayInfoMsgPage()
+		{
+			InitializeComponent();
+			NavigationPage.SetBackButtonTitle(this, "");
+			BindingContext = viewModel = new WantToCompletePayInfoMsgPageViewModel(this.Navigation);
+		}
+	}
+}
