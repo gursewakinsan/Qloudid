d66e628bccf6c255a98e1d628dbb3848187d2c4d
GPW <GPW@BST-R11>
2021-07-05 09:34:11 +0530

    working on sam

--- Qloudid/ViewModels/Pickup/PickUpAddressListPageViewModel.cs
+++ Qloudid/ViewModels/Pickup/PickUpAddressListPageViewModel.cs
@@ -1,4 +1,10 @@
 ﻿using Xamarin.Forms;
+using Qloudid.Service;
+using Qloudid.Interfaces;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
 using System.Collections.Generic;
 
 namespace Qloudid.ViewModels
@@ -29,6 +35,38 @@ namespace Qloudid.ViewModels
 			list.Add(listPickup);
 
 			ListOfPickupAddressDetail = list;
+		}
+		#endregion
+
+		#region Selected Home Address Command.
+		private ICommand selectedHomeAddressCommand;
+		public ICommand SelectedHomeAddressCommand
+		{
+			get => selectedHomeAddressCommand ?? (selectedHomeAddressCommand = new Command(async () => await ExecuteSelectedHomeAddressCommand()));
+		}
+		private async Task ExecuteSelectedHomeAddressCommand()
+		{
+			Helper.Helper.IsPickupAddress = false;
+			IDashboardService service = new DashboardService();
+			Models.EditAddressResponse address = new Models.EditAddressResponse()
+			{
+				Id = Helper.Helper.DeliveryAddressDetail.Id,
+				DeliveredAt = Helper.Helper.UserOrCompanyAddress > 1 ? 0 : 1,
+				CertificateKey = Helper.Helper.QrCertificateKey
+			};
+			int response = await service.UpdateCompanyAddressAsync(address);
+			if (Helper.Helper.IsEditDeliveryAddressFromInvoicing)
+			{
+				Helper.Helper.IsEditDeliveryAddressFromInvoicing = false;
+				await Navigation.PushAsync(new Views.ReadOnlyInvoicingAddressPage());
+			}
+			else if (Helper.Helper.IsEditAddressFromYourSignature)
+			{
+				Helper.Helper.IsEditAddressFromYourSignature = false;
+				await Navigation.PushAsync(new Views.YourSignaturePage());
+			}
+			else
+				await Navigation.PushAsync(new Views.ReadOnlyDeliveryAddressPage());
 		}
 		#endregion
 
