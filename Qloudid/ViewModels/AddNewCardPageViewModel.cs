3dae8097a3e4e59129f0538a7a4eb6d51958bca1
GPW <GPW@BST-R11>
2022-01-15 18:06:21 +0530

    Implement new design on AddNewCardPage page

--- Qloudid/ViewModels/AddNewCardPageViewModel.cs
+++ Qloudid/ViewModels/AddNewCardPageViewModel.cs
@@ -91,6 +91,19 @@ namespace Qloudid.ViewModels
 		}
 		#endregion
 
+		#region Back Button Control Command.
+		private ICommand backButtonControlCommand;
+		public ICommand BackButtonControlCommand
+		{
+			get => backButtonControlCommand ?? (backButtonControlCommand = new Command(async () => await ExecuteBackButtonControlCommand()));
+		}
+		private async Task ExecuteBackButtonControlCommand()
+		{
+			if (IsCloseShow)
+				await Navigation.PopAsync();
+		}
+		#endregion
+
 		#region Properties.
 		public bool IsCloseShow => Helper.Helper.IsAddMoreCard;
 		public string CardNumber { get; set; }
