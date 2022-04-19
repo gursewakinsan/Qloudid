45fb14b13675eec6f5ab4adbd51dd792a4137c74
GPW <GPW@BST-R11>
2022-04-19 23:22:09 +0530

    Implement new design pre checkin page

---
+++ Qloudid/ViewModels/PreCheckIn/PreCheckInPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Qloudid.ViewModels
+{
+	public class PreCheckInPageViewModel : BaseViewModel
+	{
+		#region Constructor.
+		public PreCheckInPageViewModel(INavigation navigation)
+		{
+			Navigation = navigation;
+		}
+		#endregion
+	}
+}
---
+++ Qloudid/Views/PreCheckIn/PreCheckInPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Qloudid.ViewModels;
+
+namespace Qloudid.Views.PreCheckIn
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PreCheckInPage : ContentPage
+	{
+		PreCheckInPageViewModel viewModel;
+		public PreCheckInPage()
+		{
+			InitializeComponent();
+			NavigationPage.SetBackButtonTitle(this, "");
+			BindingContext = viewModel = new PreCheckInPageViewModel(this.Navigation);
+		}
+	}
+}
