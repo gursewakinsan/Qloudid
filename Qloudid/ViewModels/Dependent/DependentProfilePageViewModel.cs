d8cf6ed9c754b3faee31c078af82fc96d9971720
GPW <GPW@BST-R11>
2022-04-30 10:17:19 +0530

    fixes

--- Qloudid/ViewModels/Dependent/DependentProfilePageViewModel.cs
+++ Qloudid/ViewModels/Dependent/DependentProfilePageViewModel.cs
@@ -25,6 +25,22 @@ namespace Qloudid.ViewModels
 		}
 		#endregion
 
+		#region Add Edit Dependent Passport Command.
+		private ICommand addEditDependentPassportCommand;
+		public ICommand AddEditDependentPassportCommand
+		{
+			get => addEditDependentPassportCommand ?? (addEditDependentPassportCommand = new Command(async () => await ExecuteAddEditDependentPassportCommand()));
+		}
+		private async Task ExecuteAddEditDependentPassportCommand()
+		{
+			Helper.Helper.DependentId = DependentDetail.Id;
+			if (DependentDetail.IsPassportUpdated)
+				await Navigation.PushAsync(new Views.Dependent.EditUploadDependentPassportPhotoPage());
+			else
+				await Navigation.PushAsync(new Views.Dependent.UploadDependentPassportPhotoPage());
+		}
+		#endregion
+
 		#region Properties.
 		public Models.DependentDetailResponse DependentDetail => Helper.Helper.DependentDetail;
 		#endregion
