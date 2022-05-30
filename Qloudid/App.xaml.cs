17c65e6b6d2aa0e0e30aac2aebc939424a58de1f
GPW <GPW@BST-R11>
2022-05-30 06:10:46 +0530

    fixes

--- Qloudid/App.xaml.cs
+++ Qloudid/App.xaml.cs
@@ -20,13 +20,13 @@ namespace Qloudid
 		{
 			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTIzNzQzQDMxMzkyZTMzMmUzMFBIaTRVTHZ6RSt5ZFl4ZzFkTkhHSWcwTGFnQ0JkUjg4TEJNcnVhSUVZeUE9");
 			InitializeComponent();
-			Helper.Helper.IsFirstTime = true;
+			/*Helper.Helper.IsFirstTime = true;
 			if (Application.Current.Properties.ContainsKey("QrCode"))
 				MainPage = new NavigationPage(new Views.DashboardPage());
 			else
-				MainPage = new NavigationPage(new Views.RestorePage());
+				MainPage = new NavigationPage(new Views.RestorePage());*/
 
-			//MainPage = new NavigationPage(new Views.SelectedIdentificatorPage());
+			MainPage = new NavigationPage(new Views.TestPage());
 		}
 
 		public void OpenAppFromWeb(string signInText)
