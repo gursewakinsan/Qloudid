8476a2407f63ceb37d2c7199d4590c68678b42f0
gursewakinsan <insagursewakinsa@gmail.com>
2022-08-28 10:23:06 +0530

    working on it

--- Qloudid/ViewModels/MyCountries/AddVisitingCountryPageViewModel.cs
+++ Qloudid/ViewModels/MyCountries/AddVisitingCountryPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Qloudid.Service;
 using Qloudid.Interfaces;
 using System.Windows.Input;
@@ -13,6 +14,19 @@ namespace Qloudid.ViewModels
 		{
 			Navigation = navigation;
 		}
+		#endregion
+
+		#region Properties.
+		public string PassportNumber { get; set; }
+		public DateTime BindIssueMinimumDate => DateTime.Today.AddYears(-70);
+		public DateTime BindIssueMaximumDate => DateTime.Today.AddDays(-1);
+		public DateTime SelectedIssueDate { get; set; }
+		public DateTime BindExpiryMinimumDate => DateTime.Today;
+		public DateTime BindExpiryMaximumDate => DateTime.Today.AddYears(70);
+		public DateTime SelectedExpiryDate { get; set; }
+
+		public byte[] UserImageData1 { get; set; }
+		public byte[] UserImageData2 { get; set; }
 		#endregion
 	}
 }
--- Qloudid/Views/MyCountries/AddVisitingCountryPage.xaml.cs
+++ Qloudid/Views/MyCountries/AddVisitingCountryPage.xaml.cs
@@ -1,12 +1,21 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.IO;
+using Plugin.Media;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Qloudid.ViewModels;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Plugin.Media.Abstractions;
 
 namespace Qloudid.Views.MyCountries
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddVisitingCountryPage : ContentPage
     {
+        int index = 0;
+		ImageSource _imageSource;
+		private IMedia _mediaPicker;
         AddVisitingCountryPageViewModel viewModel;
         public AddVisitingCountryPage()
         {
@@ -14,5 +23,136 @@ namespace Qloudid.Views.MyCountries
             NavigationPage.SetBackButtonTitle(this, "");
             BindingContext = viewModel = new AddVisitingCountryPageViewModel(this.Navigation);
         }
+		
+
+		#region Setup.
+		private async void Setup()
+		{
+			if (_mediaPicker != null) return;
+			await CrossMedia.Current.Initialize();
+			_mediaPicker = CrossMedia.Current;
+		}
+		#endregion
+
+		#region Pick Photo.
+		private async Task PickPhoto(int imageIndex)
+		{
+			Setup();
+			_imageSource = null;
+			try
+			{
+				var mediaFile = await this._mediaPicker.PickPhotoAsync(new PickMediaOptions()
+				{
+					PhotoSize = PhotoSize.Small,
+					CompressionQuality = 80
+					//CustomPhotoSize = 50
+				});
+				if (mediaFile != null)
+				{
+					if (imageIndex == 1)
+						image1.Source = ImageSource.FromStream(mediaFile.GetStream);
+					else
+						image2.Source = ImageSource.FromStream(mediaFile.GetStream);
+
+					var memoryStream = new MemoryStream();
+					await mediaFile.GetStream().CopyToAsync(memoryStream);
+					byte[] imageAsByte = memoryStream.ToArray();
+
+					if (imageIndex == 1)
+						viewModel.UserImageData1 = imageAsByte;
+					else
+						viewModel.UserImageData2 = imageAsByte;
+
+					/*_imageSource = ImageSource.FromStream(mediaFile.GetStream);
+					var memoryStream = new MemoryStream();
+					await mediaFile.GetStream().CopyToAsync(memoryStream);
+					byte[] imageAsByte = memoryStream.ToArray();
+					await Navigation.PushModalAsync(new CropView(imageAsByte, Refresh));*/
+				}
+			}
+			catch (System.Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+		#endregion
+
+		#region Take Photo.
+		private async Task TakePhoto(int imageIndex)
+		{
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+				return;
+			}
+			Setup();
+			_imageSource = null;
+			try
+			{
+				/*var mediaFile = await this._mediaPicker.TakePhotoAsync(new StoreCameraMediaOptions
+				{
+					DefaultCamera = CameraDevice.Rear
+				});*/
+
+				var mediaFile = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+				{
+					PhotoSize = PhotoSize.Medium,
+					CompressionQuality = 90,
+				});
+
+				if (imageIndex == 1)
+					image1.Source = ImageSource.FromStream(mediaFile.GetStream);
+				else
+					image2.Source = ImageSource.FromStream(mediaFile.GetStream);
+
+				var memoryStream = new MemoryStream();
+				await mediaFile.GetStream().CopyToAsync(memoryStream);
+				byte[] imageAsByte = memoryStream.ToArray();
+
+				if (imageIndex == 1)
+					viewModel.UserImageData1 = imageAsByte;
+				else
+					viewModel.UserImageData2 = imageAsByte;
+
+				/*_imageSource = ImageSource.FromStream(mediaFile.GetStream);
+				var memoryStream = new MemoryStream();
+				await mediaFile.GetStream().CopyToAsync(memoryStream);
+				byte[] imageAsByte = memoryStream.ToArray();
+				await Navigation.PushModalAsync(new CropView(imageAsByte, Refresh));*/
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+        #endregion
+
+        private async void ImageData1Clicked(object sender, EventArgs e)
+        {
+			string result = await DisplayActionSheet("Select", "Cancel", null, "Take Photo", "Pick Photo");
+			switch (result)
+			{
+				case "Take Photo":
+					await TakePhoto(1);
+					break;
+				case "Pick Photo":
+					await PickPhoto(1);
+					break;
+			}
+		}
+
+        private async void ImageData2Clicked(object sender, EventArgs e)
+        {
+			string result = await DisplayActionSheet("Select", "Cancel", null, "Take Photo", "Pick Photo");
+			switch (result)
+			{
+				case "Take Photo":
+					await TakePhoto(2);
+					break;
+				case "Pick Photo":
+					await PickPhoto(2);
+					break;
+			}
+		}
     }
 }
