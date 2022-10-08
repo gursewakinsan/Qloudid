927140265b05e280a81f19872cf604684ad58ca8
gursewakinsan <insagursewakinsa@gmail.com>
2022-10-08 12:08:35 +0530

    done

--- Qloudid/ViewModels/Consent/LandLoardConsentDetailsPageViewModel.cs
+++ Qloudid/ViewModels/Consent/LandLoardConsentDetailsPageViewModel.cs
@@ -55,6 +55,18 @@ namespace Qloudid.ViewModels
 		}
 		#endregion
 
+		#region Back Button Control Command.
+		private ICommand backButtonControlCommand;
+		public ICommand BackButtonControlCommand
+		{
+			get => backButtonControlCommand ?? (backButtonControlCommand = new Command(async () => await ExecuteBackButtonControlCommand()));
+		}
+		private async Task ExecuteBackButtonControlCommand()
+		{
+			await Navigation.PopAsync();
+		}
+		#endregion
+
 		#region Properties.
 		private Models.TenantsRequestDetail selectedTenantsDetail;
 		public  Models.TenantsRequestDetail SelectedTenantsDetail
--- Qloudid/ViewModels/Consent/LandLoardConsentPageViewModel.cs
+++ Qloudid/ViewModels/Consent/LandLoardConsentPageViewModel.cs
@@ -65,6 +65,18 @@ namespace Qloudid.ViewModels
 		}
 		#endregion
 
+		#region Back Button Control Command.
+		private ICommand backButtonControlCommand;
+		public ICommand BackButtonControlCommand
+		{
+			get => backButtonControlCommand ?? (backButtonControlCommand = new Command(async () => await ExecuteBackButtonControlCommand()));
+		}
+		private async Task ExecuteBackButtonControlCommand()
+		{
+			await Navigation.PopAsync();
+		}
+		#endregion
+
 		#region Properties.
 		private List<Models.TenantsRequestDetail> tenantsRequestDetail;
 		public List<Models.TenantsRequestDetail> TenantsRequestDetail
