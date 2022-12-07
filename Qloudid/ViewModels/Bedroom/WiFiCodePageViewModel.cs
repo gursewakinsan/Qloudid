ffb1b6064bf328635bc0f6b5747b736b61f4dad4
Gursewak Singh <Gursewak.Singh@hungerrush.com>
2022-12-07 13:22:15 -0600

    completed WiFiCodePage

---
+++ Qloudid/ViewModels/Bedroom/WiFiCodePageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Qloudid.Service;
+using Qloudid.Interfaces;
+using System.Windows.Input;
+using System.Threading.Tasks;
+
+namespace Qloudid.ViewModels
+{
+    public class WiFiCodePageViewModel : BaseViewModel
+    {
+		#region Constructor.
+		public WiFiCodePageViewModel(INavigation navigation)
+		{
+			Navigation = navigation;
+		}
+		#endregion
+	}
+}
---
+++ Qloudid/Views/Bedroom/WiFiCodePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Qloudid.ViewModels;
+
+namespace Qloudid.Views.Bedroom
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WiFiCodePage : ContentPage
+    {
+        WiFiCodePageViewModel viewModel;
+        public WiFiCodePage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "");
+            BindingContext = viewModel = new WiFiCodePageViewModel(this.Navigation);
+        }
+    }
+}
