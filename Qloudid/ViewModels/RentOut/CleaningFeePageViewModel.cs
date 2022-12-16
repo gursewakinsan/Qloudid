10655944fd889104dc4d8154d59bcfb1f468afa8
Gursewak Singh <Gursewak.Singh@hungerrush.com>
2022-12-16 01:51:34 -0600

    done

--- Qloudid/ViewModels/RentOut/CleaningFeePageViewModel.cs
+++ Qloudid/ViewModels/RentOut/CleaningFeePageViewModel.cs
@@ -12,11 +12,60 @@ namespace Qloudid.ViewModels
 		public CleaningFeePageViewModel(INavigation navigation)
 		{
 			Navigation = navigation;
+			Address = Helper.Helper.SelectedUserAddress;
+			if (Address.CleeningFeeApplicable)
+			{
+				CleaningFeeYesBg = Color.FromHex("#0C8CE8");
+				CleaningFeeNoBg = Color.Transparent;
+			}
+			else
+			{
+				CleaningFeeYesBg = Color.Transparent;
+				CleaningFeeNoBg = Color.FromHex("#0C8CE8"); 
+			}
+		}
+		#endregion
+
+		#region Cleaning Fee Command.
+		private ICommand cleaningFeeCommand;
+		public ICommand CleaningFeeCommand
+		{
+			get => cleaningFeeCommand ?? (cleaningFeeCommand = new Command<string>((selectedFee) => ExecuteCleaningFeeCommand(selectedFee)));
+		}
+		private void ExecuteCleaningFeeCommand(string selectedFee)
+		{
+			switch (selectedFee)
+            {
+				case "Yes":
+					CleaningFeeYesBg = Color.FromHex("#0C8CE8");
+					CleaningFeeNoBg = Color.Transparent;
+					break;
+				case "No":
+					CleaningFeeYesBg = Color.Transparent;
+					CleaningFeeNoBg = Color.FromHex("#0C8CE8");
+					break;
+            }
+			Address.CleeningFeeApplicable = !Address.CleeningFeeApplicable;
+		}
+		#endregion
+
+		#region Submit Cleaning Fee Command.
+		private ICommand submitCleaningFeeCommand;
+		public ICommand SubmitCleaningFeeCommand
+		{
+			get => submitCleaningFeeCommand ?? (submitCleaningFeeCommand = new Command(async () => await ExecuteSubmitCleaningFeeCommand()));
+		}
+		private async Task ExecuteSubmitCleaningFeeCommand()
+		{
+			DependencyService.Get<IProgressBar>().Show();
+			IDashboardService service = new DashboardService();
+			await Navigation.PopAsync();
+			DependencyService.Get<IProgressBar>().Hide();
 		}
 		#endregion
 
 		#region Properties.
-		private Models.EditAddressResponse address = Helper.Helper.SelectedUserAddress;
+		private Models.EditAddressResponse address;
 		public Models.EditAddressResponse Address
 		{
 			get => address;
@@ -35,6 +84,28 @@ namespace Qloudid.ViewModels
 			{
 				isPageLoad = value;
 				OnPropertyChanged("IsPageLoad");
+			}
+		}
+
+		private Color cleaningFeeNoBg;
+		public Color CleaningFeeNoBg
+		{
+			get => cleaningFeeNoBg;
+			set
+			{
+				cleaningFeeNoBg = value;
+				OnPropertyChanged("CleaningFeeNoBg");
+			}
+		}
+
+		private Color cleaningFeeYesBg;
+		public Color CleaningFeeYesBg
+		{
+			get => cleaningFeeYesBg;
+			set
+			{
+				cleaningFeeYesBg = value;
+				OnPropertyChanged("CleaningFeeYesBg");
 			}
 		}
 		#endregion
--- Qloudid/ViewModels/RentOut/SecurityDepositPageViewModel.cs
+++ Qloudid/ViewModels/RentOut/SecurityDepositPageViewModel.cs
@@ -12,11 +12,60 @@ namespace Qloudid.ViewModels
 		public SecurityDepositPageViewModel(INavigation navigation)
 		{
 			Navigation = navigation;
+			Address = Helper.Helper.SelectedUserAddress;
+			if (Address.SecurityFeeApplicable)
+			{
+				SecurityDepositYesBg = Color.FromHex("#0C8CE8");
+				SecurityDepositNoBg = Color.Transparent;
+			}
+			else
+			{
+				SecurityDepositYesBg = Color.Transparent;
+				SecurityDepositNoBg = Color.FromHex("#0C8CE8");
+			}
+		}
+		#endregion
+
+		#region Security Deposit Command.
+		private ICommand securityDepositCommand;
+		public ICommand SecurityDepositCommand
+		{
+			get => securityDepositCommand ?? (securityDepositCommand = new Command<string>((selectedFee) => ExecuteSecurityDepositCommand(selectedFee)));
+		}
+		private void ExecuteSecurityDepositCommand(string selectedFee)
+		{
+			switch (selectedFee)
+			{
+				case "Yes":
+					SecurityDepositYesBg = Color.FromHex("#0C8CE8");
+					SecurityDepositNoBg = Color.Transparent;
+					break;
+				case "No":
+					SecurityDepositYesBg = Color.Transparent;
+					SecurityDepositNoBg = Color.FromHex("#0C8CE8");
+					break;
+			}
+			Address.SecurityFeeApplicable = !Address.SecurityFeeApplicable;
+		}
+		#endregion
+
+		#region Submit Security Deposit Command.
+		private ICommand submitSecurityDepositCommand;
+		public ICommand SubmitSecurityDepositCommand
+		{
+			get => submitSecurityDepositCommand ?? (submitSecurityDepositCommand = new Command(async () => await ExecuteSubmitSecurityDepositCommand()));
+		}
+		private async Task ExecuteSubmitSecurityDepositCommand()
+		{
+			DependencyService.Get<IProgressBar>().Show();
+			IDashboardService service = new DashboardService();
+			await Navigation.PopAsync();
+			DependencyService.Get<IProgressBar>().Hide();
 		}
 		#endregion
 
 		#region Properties.
-		private Models.EditAddressResponse address = Helper.Helper.SelectedUserAddress;
+		private Models.EditAddressResponse address;
 		public Models.EditAddressResponse Address
 		{
 			get => address;
@@ -35,6 +84,28 @@ namespace Qloudid.ViewModels
 			{
 				isPageLoad = value;
 				OnPropertyChanged("IsPageLoad");
+			}
+		}
+
+		private Color securityDepositNoBg;
+		public Color SecurityDepositNoBg
+		{
+			get => securityDepositNoBg;
+			set
+			{
+				securityDepositNoBg = value;
+				OnPropertyChanged("SecurityDepositNoBg");
+			}
+		}
+
+		private Color securityDepositYesBg;
+		public Color SecurityDepositYesBg
+		{
+			get => securityDepositYesBg;
+			set
+			{
+				securityDepositYesBg = value;
+				OnPropertyChanged("SecurityDepositYesBg");
 			}
 		}
 		#endregion
