1d1bc07f1d5c1111e3da4ed3898d54f7cdc9d8fe
Gursewak Singh <Gursewak.Singh@hungerrush.com>
2023-01-08 23:07:36 -0600

    fixs

--- Qloudid/ViewModels/RentOut/RentOutPageViewModel.cs
+++ Qloudid/ViewModels/RentOut/RentOutPageViewModel.cs
@@ -40,7 +40,7 @@ namespace Qloudid.ViewModels
 			if (IsApartment && IsArrivalAndRulesIconChecked && IsPhotoTextAndAvailability && IsPricing && IsGetStarted)
 				IsReadyToPublish = true;
 			else
-				IsReadyToPublish = true;
+				IsReadyToPublish = false;
 			DependencyService.Get<IProgressBar>().Hide();
             IsPageLoad = true;
         }
