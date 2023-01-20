03ebec9cbacd29ce692125777d0421e300d342be
Gursewak Singh <Gursewak.Singh@hungerrush.com>
2023-01-20 12:12:04 -0600

    Done

---
+++ Qloudid/ViewModels/RentOut/AddNewGuestDetailsPageViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Xamarin.Forms;
+using Qloudid.Service;
+using Qloudid.Interfaces;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Qloudid.ViewModels
+{
+    public class AddNewGuestDetailsPageViewModel : BaseViewModel
+    {
+		#region Constructor.
+		public AddNewGuestDetailsPageViewModel(INavigation navigation)
+		{
+			Navigation = navigation;
+			Address = Helper.Helper.SelectedUserAddress;
+			CountryList = new ObservableCollection<Models.Country>(Helper.Helper.CountryList);
+			SelectedCountry = CountryList[0];
+			CountryCodeList = new ObservableCollection<Models.Country>(Helper.Helper.CountryList);
+			SelectedCountryCode = CountryCodeList[0];
+		}
+		#endregion
+
+		#region Properties.
+		private Models.EditAddressResponse address;
+		public Models.EditAddressResponse Address
+		{
+			get => address;
+			set
+			{
+				address = value;
+				OnPropertyChanged("Address");
+			}
+		}
+
+		private ObservableCollection<Models.Country> countryList;
+		public ObservableCollection<Models.Country> CountryList
+		{
+			get => countryList;
+			set
+			{
+				countryList = value;
+				OnPropertyChanged("CountryList");
+			}
+		}
+
+		private Models.Country selectedCountry;
+		public Models.Country SelectedCountry
+		{
+			get => selectedCountry;
+			set
+			{
+				selectedCountry = value;
+				OnPropertyChanged("SelectedCountry");
+			}
+		}
+
+		private ObservableCollection<Models.Country> countryCodeList;
+		public ObservableCollection<Models.Country> CountryCodeList
+		{
+			get => countryCodeList;
+			set
+			{
+				countryCodeList = value;
+				OnPropertyChanged("CountryCodeList");
+			}
+		}
+
+		private Models.Country selectedCountryCode;
+		public Models.Country SelectedCountryCode
+		{
+			get => selectedCountryCode;
+			set
+			{
+				selectedCountryCode = value;
+				OnPropertyChanged("SelectedCountryCode");
+			}
+		}
+
+		public DateTime BindIssueMinimumDate => DateTime.Today.AddYears(-70);
+		public DateTime BindIssueMaximumDate => DateTime.Today.AddDays(-1);
+		public DateTime SelectedIssueDate { get; set; }
+		public DateTime BindExpiryMinimumDate => DateTime.Today;
+		public DateTime BindExpiryMaximumDate => DateTime.Today.AddYears(70);
+		public DateTime SelectedExpiryDate { get; set; }
+		public byte[] UserImageDataFront { get; set; }
+		public byte[] UserImageDataBack { get; set; }
+		#endregion
+	}
+}
---
+++ Qloudid/Views/RentOut/AddNewGuestDetailsPage.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using Plugin.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Qloudid.ViewModels;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Plugin.Media.Abstractions;
+
+namespace Qloudid.Views.RentOut
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddNewGuestDetailsPage : ContentPage
+    {
+        #region Variables.
+        private IMedia _mediaPicker;
+        ImageSource _imageSource;
+        AddNewGuestDetailsPageViewModel viewModel;
+        #endregion
+
+        #region Constructor.
+        public AddNewGuestDetailsPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "");
+            BindingContext = viewModel = new AddNewGuestDetailsPageViewModel(this.Navigation);
+        }
+		#endregion
+
+		#region Setup.
+		private async void Setup()
+		{
+			if (_mediaPicker != null) return;
+			await CrossMedia.Current.Initialize();
+			_mediaPicker = CrossMedia.Current;
+		}
+		#endregion
+
+		#region Pick Photo.
+		private async Task PickPhoto(int index)
+		{
+			Setup();
+			_imageSource = null;
+			try
+			{
+				var mediaFile = await this._mediaPicker.PickPhotoAsync(new PickMediaOptions()
+				{
+					PhotoSize = PhotoSize.Small,
+					CompressionQuality = 80
+					//CustomPhotoSize = 50
+				});
+				if (mediaFile != null)
+				{
+					var memoryStream = new MemoryStream();
+					await mediaFile.GetStream().CopyToAsync(memoryStream);
+					byte[] imageAsByte = memoryStream.ToArray();
+					if (index == 1)
+					{
+						imgBtnIdPhotoFront.Source = ImageSource.FromStream(mediaFile.GetStream);
+						imgBtnIdPhotoFront.IsVisible = true;
+						btnIdPhotoFront.IsVisible = false;
+						viewModel.UserImageDataFront = imageAsByte;
+					}
+					else
+					{
+						imgBtnIdPhotoBack.Source = ImageSource.FromStream(mediaFile.GetStream);
+						imgBtnIdPhotoBack.IsVisible = true;
+						btnIdPhotoBack.IsVisible = false;
+						viewModel.UserImageDataBack = imageAsByte;
+					}
+				}
+			}
+			catch (System.Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+		#endregion
+
+		#region Take Photo.
+		private async Task TakePhoto(int index)
+		{
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+				return;
+			}
+			Setup();
+			_imageSource = null;
+			try
+			{
+				/*var mediaFile = await this._mediaPicker.TakePhotoAsync(new StoreCameraMediaOptions
+				{
+					DefaultCamera = CameraDevice.Rear
+				});*/
+
+				var mediaFile = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+				{
+					PhotoSize = PhotoSize.Medium,
+					CompressionQuality = 90,
+				});
+
+				var memoryStream = new MemoryStream();
+				await mediaFile.GetStream().CopyToAsync(memoryStream);
+				byte[] imageAsByte = memoryStream.ToArray();
+
+				if (index == 1)
+				{
+					imgBtnIdPhotoFront.Source = ImageSource.FromStream(mediaFile.GetStream);
+					imgBtnIdPhotoFront.IsVisible = true;
+					btnIdPhotoFront.IsVisible = false;
+					viewModel.UserImageDataFront = imageAsByte;
+				}
+				else
+				{
+					imgBtnIdPhotoBack.Source = ImageSource.FromStream(mediaFile.GetStream);
+					imgBtnIdPhotoBack.IsVisible = true;
+					btnIdPhotoBack.IsVisible = false;
+					viewModel.UserImageDataBack = imageAsByte;
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+		#endregion
+
+		#region On Id Photo Front Clicked.
+		private async void OnIdPhotoFrontClicked(object sender, EventArgs e)
+        {
+			string result = await DisplayActionSheet("Select", "Cancel", null, "Take Photo", "Pick Photo");
+			switch (result)
+			{
+				case "Take Photo":
+					await TakePhoto(1);
+					break;
+				case "Pick Photo":
+					await PickPhoto(1);
+					break;
+			}
+		}
+		#endregion
+
+		#region On Id Photo Back Clicked.
+		private async void OnIdPhotoBackClicked(object sender, EventArgs e)
+		{
+			string result = await DisplayActionSheet("Select", "Cancel", null, "Take Photo", "Pick Photo");
+			switch (result)
+			{
+				case "Take Photo":
+					await TakePhoto(2);
+					break;
+				case "Pick Photo":
+					await PickPhoto(2);
+					break;
+			}
+		}
+		#endregion
+	}
+}
