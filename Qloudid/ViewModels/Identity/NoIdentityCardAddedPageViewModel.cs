b77ca6f1d30e24aa81c9f144f8ac78c3988834a0
gursewakinsan <insagursewakinsa@gmail.com>
2023-03-21 10:05:59 +0530

    Add new Page no identity

---
+++ Qloudid/ViewModels/Identity/NoIdentityCardAddedPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using System.Windows.Input;
+using System.Threading.Tasks;
+
+namespace Qloudid.ViewModels
+{
+    public class NoIdentityCardAddedPageViewModel : BaseViewModel
+    {
+        #region Constructor.
+        public NoIdentityCardAddedPageViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+
+        #region Add Identity card Command.
+        private ICommand addIdentityCardCommand;
+        public ICommand AddIdentityCardCommand
+        {
+            get => addIdentityCardCommand ?? (addIdentityCardCommand = new Command(async () => await ExecuteAddIdentityCardCommand()));
+        }
+        private async Task ExecuteAddIdentityCardCommand()
+        {
+            await Navigation.PushAsync(new Page());
+        }
+        #endregion
+    }
+}
---
+++ Qloudid/Views/Identity/NoIdentityCardAddedPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Qloudid.ViewModels;
+
+namespace Qloudid.Views.Identity
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoIdentityCardAddedPage : ContentPage
+    {
+        NoIdentityCardAddedPageViewModel viewModel;
+        public NoIdentityCardAddedPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "");
+            BindingContext = viewModel = new NoIdentityCardAddedPageViewModel(this.Navigation);
+        }
+    }
+}
